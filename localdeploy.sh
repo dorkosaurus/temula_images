069a7f88dc36ab6a17f3a1b06f0d1c5c35910eb8
Rambo Swampy <vramaswamy@gmail.com>
2012-06-09 22:48:19 -0700

    bumped the version

--- localdeploy.sh
+++ localdeploy.sh
@@ -2,6 +2,6 @@
 rm -rf /Applications/apache-tomcat-6.0.35/webapps/temula_images/*;
 rm -rf /Applications/apache-tomcat-6.0.35/webapps/temula_images.war
 mvn clean package;
-mv target/temula_images-0.0.1-SNAPSHOT.war target/temula_images.war;
+mv target/temula_images-0.0.2-SNAPSHOT.war target/temula_images.war;
 cp target/temula_images.war /Applications/apache-tomcat-6.0.35/webapps/;
 /Applications/apache-tomcat-6.0.35/bin/startup.sh;
